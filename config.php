3bb771d1ba3316c0b8951cc4e4a6fe410ab436be
Andhryws <andhrywsg@gmail.com>
2025-02-07 08:00:45 -0300

    primeiro commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'financienciasDB';
+
+
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+?>
---
+++ home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>financiencia</title>
+    <style>
+     body{
+        font-family: Arial, hELVETICA, sans-serif;
+        background: linear-gradient(to right, rgb(20,147,220), rgb(17,54,71));
+        text-align: center;
+        color: white;
+     }
+      .box{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color:rgba(0, 0, 0, 0.7);
+        padding: 30px;
+        border-radius: 10px;
+      }
+      a{
+        text-decoration: none;
+        color: white;
+        border: 3px solid dodgerblue;
+        border-radius: 10px;
+        padding: 10px;
+      }
+      a:hover{
+        background-color: dodgerblue;
+      }
+    </style>
+</head>
+<body>
+    <a href="http://127.0.0.1:5500/pg1.html">Voltar</a>
+    <h1>Financiência</h1>
+    <h2>Página inicial</h2>
+    <div class="box">
+        <a href="index.php">Login</a>
+        <a href="cadastro.php">Cadastre-se</a>
+</body>
+</html>
